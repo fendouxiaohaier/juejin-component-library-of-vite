dfdf3f44fc93b9019f8d0f051769356dfd244c83
Li Yang <cdjv_liyang@163.com>
2023-03-22 23:12:40 +0800

    feat: 将组建封装为组件库

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+
+import SButton from "./button";
+import SFCButton from "./button/SFCButton.vue";
+import JSXButton from "./button/JSXButton";
+
+createApp(JSXButton).mount("#app");
