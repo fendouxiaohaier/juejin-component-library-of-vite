4c86ef51fd8295457dbd46963ff2f770e76d6171
Li Yang <cdjv_liyang@163.com>
2023-03-30 23:02:54 +0800

    feat: 配置eslint_husky

--- src/button/tests/Button.spec.ts
+++ src/button/tests/Button.spec.ts
@@ -4,40 +4,49 @@ import { shallowMount } from "@vue/test-utils";
 import { describe, expect, test } from "vitest";
 
 // 测试分组
-describe('测试Button', () => {
-	// mount
-	test("mount  @vue/test-utils", () => {
-		// @vue/test-utils
-		const wrapper = shallowMount(Button, {
-			slots: {
-				default: 'Buttona'
-			}
-		});
+describe("测试Button", () => {
+  // mount
+  test("mount  @vue/test-utils", () => {
+    // @vue/test-utils
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Buttona",
+      },
+    });
 
-		// 断言
-		expect(wrapper.text()).toBe("Button");
+    // 断言
+    expect(wrapper.text()).toBe("Button");
+  });
+});
 
-	});
-})
-
-describe('测试 button 的 color', () => {
-	test("测试默认颜色default", () => {
-		const wrapper = shallowMount(Button, {
-			slots: {
-				default: 'Button'
-			}
-		});
-		expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-500')).toBe(true)
-	});
-	test("测试红色red", () => {
-		const wrapper = shallowMount(Button, {
-			slots: {
-				default: 'Button'
-			},
-			props: {
-				color: 'red'
-			}
-		});
-		expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-red-500')).toBe(true)
-	});
-})
+describe("测试 button 的 color", () => {
+  test("测试默认颜色default", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-500")
+    ).toBe(true);
+  });
+  test("测试红色red", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        color: "red",
+      },
+    });
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-red-500")
+    ).toBe(true);
+  });
+});
